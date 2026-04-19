b28e75ace20b3fa47f5644f764a1d794ce47ccd2
MartyIX <203266+MartyIX@users.noreply.github.com>
2026-04-19 21:41:25 +0200

    fix

--- src/utils/boltzClient.ts
+++ src/utils/boltzClient.ts
@@ -573,9 +573,10 @@ export const broadcastTransaction = async (
     const promises: Promise<{
         id: string;
     }>[] = [
-            // See https://github.com/BoltzExchange/boltz-web-app/blob/v1.2.1/src/helper.js#L236
-        fetcher<{ id: string }>(`/broadcasttransaction`, { hex: txHex, }),
-        broadcastToExplorer(asset, txHex),
+        // See https://github.com/BoltzExchange/boltz-web-app/blob/v1.2.1/src/helper.js#L236
+        fetcher<{ id: string }>(`/broadcasttransaction`, { currency: asset, transactionHex: txHex, }),
+        // TODO.
+        // broadcastToExplorer(asset, txHex),
     ];
 
     const results = await Promise.allSettled(promises);
