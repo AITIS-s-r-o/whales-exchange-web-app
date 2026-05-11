51993896fd408f66c303ccfea18f3e15d301c10a
dejfcz <dejfcz@users.noreply.github.com>
2026-05-11 12:36:41 +0100

    info

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -158,7 +158,8 @@ const dict = {
         select_asset: "Select {{ direction }} Asset",
         tx_confirmed: "Transaction confirmed",
         tx_ready_to_claim: "Claiming transaction now...",
-        wex_tx_ready_to_claim_forward: "Swap provider should pay the invoice now...",
+        wex_tx_ready_to_claim_forward:
+            "The swap provider should pay the invoice now. After you receive the payment, the swap provider provider will claim the on-chain funds which will complete the swap.",
         refunded: "Swap has been refunded successfully!",
         locktime_not_satisfied: "Locktime requirement not satisfied",
         already_refunded: "Swap already refunded",
